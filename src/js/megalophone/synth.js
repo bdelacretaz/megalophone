82a2525be89eae1aac48c82fd3b0e4dd198bdb59
Bertrand Delacretaz <bdelacretaz@apache.org>
2025-10-02 09:42:25 +0200

    better synth

--- src/js/megalophone/synth.js
+++ src/js/megalophone/synth.js
@@ -5,30 +5,30 @@ export default class Synth {
   constructor(ctx) {
 
     this.#ctx = ctx;
-    for (let i = 1; i < 6; i++) {
+
+    const filter = new BiquadFilterNode(ctx);
+    const gain = new GainNode(ctx);
+
+    const lfo = new OscillatorNode(ctx);
+    lfo.type = 'sawtooth';
+    lfo.frequency.value = 5;
+    lfo.start();
+    const lfoGain = new GainNode(ctx);
+    lfoGain.gain.value = 100;
+    filter.frequency.value = 1800;
+    filter.Q.value = 0.2;
+    lfo.connect(lfoGain);
+    lfoGain.connect(filter.frequency);
+    gain.connect(filter);
+    filter.connect(ctx.destination);
+
+    for (let i = 1; i < 21; i++) {
       const osc = new OscillatorNode(ctx);
       this.#oscillators.push(osc);
-      osc.type = 'sawtooth';
+      osc.type = i % 2 == 0 ? 'square': 'sawtooth';
       osc.frequency.setValueAtTime(440, ctx.currentTime);
-      osc.detune.value = 0.2*i;
-
-      const filter = new BiquadFilterNode(ctx);
-      const gain = new GainNode(ctx);
-
-      const lfo = new OscillatorNode(ctx);
-      lfo.type = 'sawtooth';
-      lfo.frequency.value = 5;
-      lfo.start();
-      const lfoGain = new GainNode(ctx);
-      lfoGain.gain.value = 100;
-      filter.frequency.value = 2500;
-      filter.Q.value = 2;
-      lfo.connect(lfoGain);
-      lfoGain.connect(filter.Q);
-
+      osc.detune.value = 2 * i;
       osc.connect(gain);
-      gain.connect(filter);
-      filter.connect(ctx.destination);
     }
   }
 
